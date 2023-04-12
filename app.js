e90ae5978ec65dd4d59cbad0357f1fe336faa922
Jacob Phillips <0123phillips@gmail.com>
2023-04-12 08:43:42 +0100

    makes a stream-lined express app, exports as app for server

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //initialise all dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
 const path = require('path');
 
 //import routers
